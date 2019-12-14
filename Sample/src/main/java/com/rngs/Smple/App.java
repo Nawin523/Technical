35cdfb0e32c3a0539a47dc645333fa9f041b53d8
Nawin523 <Misalanawin@gmail.com>
2019-12-14 14:16:57 +0530

    First commit

---
+++ Sample/src/main/java/com/rngs/Smple/App.java
@@ -0,0 +1,27 @@
+package com.rngs.Smple;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int i,j,k;
+    	
+    	Scanner sn = new Scanner(System.in);
+    	System.out.print("Enter the value of i :");
+    	i= sn.nextInt();
+    	System.out.print("Enter the value of j :");
+    	j=sn.nextInt();
+    	//i=20;j=25;
+    	k=i+j;
+    	
+    	System.out.println("addition of i and j is :" +k);
+    	
+        System.out.println( "Hello Vasudha Meesala!" );
+    }
+}
